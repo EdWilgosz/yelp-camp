e98fe3968635dd0ad8e00d8ee0f2f33172f22e17
Ed Wilgosz <EdWilgosz@gmail.com>
2019-09-10 18:43:41 -0400

    change to MongoDBAtlas from localMongodb

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true, useFindAndModify: false });
+// mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect("mongodb+srv://EdWilgosz:<Eddie0505>@cluster0-jvpcl.mongodb.net/yelp_camp?retryWrites=true&w=majority", { useNewUrlParser: true, useFindAndModify: false });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
