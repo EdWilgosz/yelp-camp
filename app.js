573a75fee643c82a5e4b125ca2af323af646a785
Ed Wilgosz <EdWilgosz@gmail.com>
2019-09-10 17:51:43 -0400

    fix typo in app.js listener

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 
 
-app.listen(process.env.PORT || 3000, proces.env.IP, function(){
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
     console.log("YelpCamp Server has started!")
 });
